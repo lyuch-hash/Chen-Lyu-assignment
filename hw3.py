5f462414065963b4e2beff1e8e16ae4385c78e1e
lyuch-hash <78378422+lyuch-hash@users.noreply.github.com>
2021-02-12 23:15:13 -0500

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 12 20:39:05 2021
+
+@author: maidawu
+"""
+
+import random
+print(random.randint(0, 9))
+string='ldldldld'
+print(string[2:])
+for i in string:
+    print(i)
+lst=list()
+if (len(lst)==0):
+    print('shit')
